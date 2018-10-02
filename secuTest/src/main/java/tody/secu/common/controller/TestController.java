bd653a3c32fbe55ed5a52d319c5c2e8caf191782
todyTech <y0unn@hanmail.net>
2018-10-02 19:04:54 +0900

    feat: Add logout controller

--- secuTest/src/main/java/tody/secu/common/controller/TestController.java
+++ secuTest/src/main/java/tody/secu/common/controller/TestController.java
@@ -1,5 +1,8 @@
 package tody.secu.common.controller;
 
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpSession;
+
 import org.apache.log4j.Logger;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -35,6 +38,15 @@ public class TestController {
     public String page() throws Exception {
     	//ModelAndView mv = new ModelAndView("/page");
     	return "/page";
+    }
+    
+    @RequestMapping(value="/logout")
+    public ModelAndView logout(HttpServletRequest req, HttpSession session) throws Exception {
+    	session.invalidate();
+    	ModelAndView mv = new ModelAndView("/goIndex");
+    	/* mv.addObject("msg", "로그아웃");
+    	mv.addObject("url", "/"); */
+    	return mv;
     }
 
 }
