7943203150e36d6b47a2f7a0021c9ba8bd21c145
todyTech <y0unn@hanmail.net>
2018-10-08 15:01:43 +0900

    feat: Update matchPassword
    암호화된 비밀번호 비교를 하고 싶었지만, DB에 저장된 비밀번호가 String 이기에 AuthenticationProvider에서
    matchPassword() 메소드를 만들어서 비교해주었다.(임시방편)

--- secuTest/src/main/java/tody/secu/common/service/UserAuthenticationProvider.java
+++ secuTest/src/main/java/tody/secu/common/service/UserAuthenticationProvider.java
@@ -6,11 +6,14 @@ import java.util.Collections;
 import org.apache.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.authentication.AuthenticationProvider;
+import org.springframework.security.authentication.BadCredentialsException;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.AuthenticationException;
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.userdetails.User;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 import tody.secu.common.vo.UserDetailsVO;
 
@@ -20,6 +23,9 @@ public class UserAuthenticationProvider implements AuthenticationProvider {
 	
 	@Autowired
 	private UserDetailService userDeSer;
+	
+	@Autowired
+	private BCryptPasswordEncoder passwordEncoder;
 	
 	@SuppressWarnings("unchecked")
 	@Override
@@ -37,6 +43,10 @@ public class UserAuthenticationProvider implements AuthenticationProvider {
 		user = (UserDetailsVO)userDeSer.loadUserByUsername(username);
 		log.debug("authenticationProvider::::: DB :: "+user.getUsername()+" / "+user.getPassword());
 		
+		//if( !passwordEncoder.matches(password, user.getPassword()))
+		if( !matchPassword(password,user.getPassword()))
+			throw new BadCredentialsException("no password");
+		
 		authorities = (Collection<GrantedAuthority>) user.getAuthorities();
 		
 		return new UsernamePasswordAuthenticationToken(username, password, authorities);
@@ -46,6 +56,11 @@ public class UserAuthenticationProvider implements AuthenticationProvider {
 	public boolean supports(Class<?> authentication) {
 		// TODO Auto-generated method stub
 		return true;
+	}
+	
+	@SuppressWarnings("unused")
+	private boolean matchPassword(String loginPwd, String password) {
+		return loginPwd.equals(password);
 	}
 
 }
